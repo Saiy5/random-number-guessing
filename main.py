4d7a61939161a1eb36e6e723826f04d099639a99
Suman Mondal <141320503+Saiy5@users.noreply.github.com>
2025-08-22 16:40:23 +0530

    fix while loop and error handling

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ def guess_the_number():
     print("Welcome to the number Guessing game!")
     n = random.randint(1, 100)  #Generate a random number between 1 to 100
     guesses = 0
+    is_running = True
 
-    while True: #This conditon should always be true
+    while is_running: #This conditon should always be true
         try:
             a = int(input("Guess the number between 1 to 100: "))
             guesses +=1  #Incorrect number count
 
-            if(a>n):
+            if(a > n):
                 print("Lower Number Please")
                 
-            elif(a<n):
+            elif(a < n):
                 print("Higher Number please")
 
             else:
                 print(f"You have guessed the number {n} correctly in {guesses} attempts")
-                break #Exit the loop when you guess the number
+                is_running = False #Exit the loop when you guess the number
 
-        except ValueError as v:
+        except ValueError:
             print("Please enter a valid number")
         except KeyboardInterrupt:
             print("\nProgram interrupted. Goodbye!")
